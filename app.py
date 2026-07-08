82446b7d7307ab677ce17ac05bbb25e7c2409541
musa3mk-stack <musa3mk@gmail.com>
2026-07-08 09:49:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.middleware.proxy_fix import ProxyFix
 from authlib.integrations.flask_client import OAuth
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Templates')
 
 # ---------------------------------------------------------------------------
 # SECURITY / DEPLOYMENT CONFIG
